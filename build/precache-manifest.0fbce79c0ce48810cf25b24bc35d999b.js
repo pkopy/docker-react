cc31c8e534030edadcb0968d86e88e65e3603e5d
Paweł Kopycki <p.kopycki@radwag.pl>
2019-10-16 14:52:11 +0200

    init

---
+++ build/precache-manifest.0fbce79c0ce48810cf25b24bc35d999b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a01a0bac7084bae79e6b21b1daea34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3078f4958e5150395c01",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "14f7ba62d534ca10bdba",
+    "url": "/static/js/2.8e9d5bf3.chunk.js"
+  },
+  {
+    "revision": "3078f4958e5150395c01",
+    "url": "/static/js/main.8e38895c.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
